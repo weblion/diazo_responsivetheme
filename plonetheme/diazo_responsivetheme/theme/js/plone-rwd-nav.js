e0aee2054ce6e31be710e640392a8f4814541a00
Rob Porter <robzonenet@gmail.com>
2013-08-09 13:11:32 -0400

    changed the width to 610px in the js to match the css break down point

--- plonetheme/diazo_responsivetheme/theme/js/plone-rwd-nav.js
+++ plonetheme/diazo_responsivetheme/theme/js/plone-rwd-nav.js
@@ -9,16 +9,13 @@ Changes made by Jochen Dekeyser
 Last Updated: August 6, 2013
 */
 
-function below_600(){
-      // on load add the li if less than 600 px wide and the nav-menu class
+function below_610(){
+      // on load add the li if less than 610 px wide and the nav-menu class
       if(window.location.hash != '#portal-globalnav') {
-        if ($(window).width() < 600) {
-
+        if ($(window).width() < 610) {
             var showNavigationLabel = _('Show Navigation');
             var hideNavigationLabel = _('Hide Navigation');
-            // alert("showNavigationLabel=" + showNavigationLabel + ", hideNavigationLabel=" + hideNavigationLabel);
-            
-            
+
             // always show right Navigation label for button, even in rare situations:
             // e.g. expand the mobile menu, enlarge the browser so the mobile menu disappears,
             // make the browser smaller and see the right label with the expanded menu again
@@ -55,33 +52,32 @@ function below_600(){
 $(document).ready(function() {
     // we need to get to the javascript variables that are created by pyton so we can't use the html file since it puts in the ++theme++ stuff
     $("head").append('<script type="text/javascript" src="./portal_javascripts/Sunburst%20Theme/plone_javascript_variables.js"></script>');
-                // i18n
-            jarn.i18n.loadCatalog('plonetheme.diazo_responsivetheme');
-            _ = jarn.i18n.MessageFactory('plonetheme.diazo_responsivetheme');
-      // add a div around globalnav
-        $('ul#portal-globalnav').wrap('<div class="nav-primary" />');
-        if ( $(".navigation-button").length == 0 ) {
-            below_600();
-        }
-        $(window).resize(function() {
-            // if window is smaller than 600px put in the extra li
-            if ($(window).width() <= 600) {
-                 // check to see if we have this particular li.. if not add it
-                 if ( $(".navigation-button").length == 0 ) {
-                   below_600();
-                 }
+    // i18n
+    jarn.i18n.loadCatalog('plonetheme.diazo_responsivetheme');
+    _ = jarn.i18n.MessageFactory('plonetheme.diazo_responsivetheme');
+    // add a div around globalnav
+    $('ul#portal-globalnav').wrap('<div class="nav-primary" />');
+    if ( $(".navigation-button").length == 0 ) {
+        below_610();
+    }
+    $(window).resize(function() {
+        // if window is smaller than 610px put in the extra li
+        if ($(window).width() <= 610) {
+             // check to see if we have this particular li.. if not add it
+             if ( $(".navigation-button").length == 0 ) {
+               below_610();
              }
-            // if window is bigger take away li
-            if ($(window).width() > 600) {
-                 // check and see if .navigation-button is there.. if so remove it
-                 if ( $(".navigation-button").length > 0 ) {
-                     $('#portal-top').removeClass('nav-menu');
-                     $('.navigation-button').remove();
-                     $('.nav-section').remove();
-                 }
-            }
-
-        });
+         }
+        // if window is bigger take away li
+        if ($(window).width() > 610) {
+             // check and see if .navigation-button is there.. if so remove it
+             if ( $(".navigation-button").length > 0 ) {
+                 $('#portal-top').removeClass('nav-menu');
+                 $('.navigation-button').remove();
+                 $('.nav-section').remove();
+             }
+        }
+    });
    
 
 });
